d08650f641bc675e3e774dbb136e03454eb08b2a
xuxinlei <381959437@qq.com>
2022-05-13 16:58:43 +0800

    第二次提交

--- Test1/src/main/java/com/example/test1/Test1Application.java
+++ Test1/src/main/java/com/example/test1/Test1Application.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test1Application {
 
     public static void main(String[] args) {
-//        SpringApplication.run(Test1Application.class, args);
-        System.out.println("sss");
+        System.out.println("测试成功！");
     }
 
 }
