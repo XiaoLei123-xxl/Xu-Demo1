ea2fabaacd748e5b6080383500b95d0142934e7d
xuxinlei <381959437@qq.com>
2022-05-13 17:06:32 +0800

    第三次

--- Test1/src/main/java/com/example/test1/Test1Application.java
+++ Test1/src/main/java/com/example/test1/Test1Application.java
@@ -1,12 +1,15 @@
 package com.example.test1;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test1Application {
 
     public static void main(String[] args) {
+
+        System.out.println("测试成功！");
+
+        System.out.println("测试成功！");
         System.out.println("测试成功！");
     }
 
